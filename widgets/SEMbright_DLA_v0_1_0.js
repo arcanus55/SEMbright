0f972c72b05b6d0a7253ffb4e1109bd2d18c6ea7
neodigm <3151842+neodigm@users.noreply.github.com>
2022-03-02 09:22:53 -0600

    semb_widget

---
+++ widgets/SEMbright_DLA_v0_1_0.js
@@ -0,0 +1,102 @@
+/*
+SEMbright DLA v0.1.0
+Copyright (c) 2022, Arcanus 55 Privacy Paranoid Vault | Forged by Scott C. Krause
+*/
+
+//  SEMbright Options Custom Config Begin  //
+let sembOpt = {
+  CONSOLE_LOG_VER: true,
+  SEMB_DEBUG_lOG: false
+}
+
+if( typeof sembOptCustom != 'undefined' ){
+    for( cnfgProp in sembOptCustom ){  //  Import Custom Objects props if exists
+      sembOpt[ cnfgProp ] = sembOptCustom[ cnfgProp ]
+    }
+}
+
+//  SEMbright Utils Begin  //
+const sembUtils = ( ( _d ) =>{
+  return {
+    ver: "0.1.0",
+    isMobile: function(){ return (_d.body.clientWidth <= 768) ? true : false; },
+    f1210: function(){ return (Math.floor(Math.random() * (10) + 1)); },  //  1 to 10
+    f02x: function(x){ return (Math.floor(Math.random() * x)); },  //  0 to x
+    fAsyncJS: function( _d, _uri, bJS, _cb ){  //  Load JS/CSS Async then callback
+      //  let bJS = ( _uri.indexOf(".js") != -1)
+      let el = _d.createElement( bJS ? "script" : "link" )
+      if( bJS ){
+        el.type = "text/javascript"
+        el.async = true
+        el.src = _uri        
+      }else{
+        el.rel = "stylesheet"
+        el.href = _uri
+      }
+
+      if( _cb ) el.onload = function(){ _cb() }
+      _d.getElementsByTagName( "head" )[0].appendChild( el )
+    },
+    data2prop: function( sDset ){  //  Convert HTML data attrib name to JS dataset name
+      sDset = sDset.replace("data-", "").toLowerCase();
+      let aDset = sDset.split(""), aDret = [], bUpper = false;
+      aDset.forEach( (sVal, nIx) => {
+          if( sVal == "-" ){
+              bUpper = true;
+          }else{
+              aDret.push( ( bUpper ) ? sVal.toUpperCase() : sVal );
+              bUpper = false;
+          }
+      });
+      return aDret.join("");
+    },
+    capFirst: s => (s && s[0].toUpperCase() + s.slice(1)) || ""
+  }
+})( document );
+
+//  SEMbright Begin  //
+class SEMbright {
+    constructor( _d, _aQ ) {
+        this._d = _d; this._aQ = _aQ
+        this.bIsInit = false; this.bIsPause = false
+    }
+    preInit (){
+      //  Request CSS (gh-pages?)
+      //  If N55 else request N55
+      //  Might want to point to the CDN at some point
+      sembUtils.fAsyncJS( document, "https://arcanus55.github.io/neodigm55/dist/neodigm55_v1_9.js", true, function(){
+        sembUtils.fAsyncJS( document, "https://arcanus55.github.io/neodigm55/dist/neodigm55_v1_9.css", false, function(){
+          console.log("ducks")
+          //window.doDOMContentLoaded()
+  if( doDOMContentLoaded ) doDOMContentLoaded()
+
+        })
+      })
+    }
+    init () {
+        this.bIsInit = true
+        return this
+    }
+    pause (){ this.bIsPause = true; return this; }
+    play (){ this.bIsPause = false; return this; }
+    setTheme (){ if( this.bIsInit ){ return this; } }
+}
+let sembright = new SEMbright( document, ["neodigm-claire"] )
+
+
+document.addEventListener("DOMContentLoaded", function(ev) {
+  const neodigmMU = `
+   <template id="semb--wdla__id" data-n55-sodapop-modal="false" 
+  data-n55-sodapop-size="medium">
+  
+  YOUR CONTENT HERE
+  
+  </template>`;
+  let eMU = document.createElement("output");
+  eMU.innerHTML = neodigmMU;
+  document.body.appendChild(eMU);
+  setTimeout( ()=>{
+    sembright.preInit()
+    if( sembOpt.CONSOLE_LOG_VER ) console.log("%c SEMbright White-label Digital Landscape Audit ✨ v" + sembUtils.ver, "background: #000; color: #F5DF4D; font-size: 20px");
+  }, 56)
+});
